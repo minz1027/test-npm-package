a679ec653adc8d39e3f5fbb57fe4905ddb2afa30
Min Zhang <minzhang@andrew.cmu.edu>
2017-03-06 14:21:40 -0800

    Update set-env.sh

--- set-env.sh
+++ set-env.sh
@@ -1,1 +1,1 @@
-curl -H "Authorization: Bearer $SD_TOKEN" -H "Content-Type: application/json" https://api.screwdriver.cd/v4/pipelines
+curl -H "Authorization: Bearer $SD_TOKEN" -H "Content-Type: application/json" https://api.screwdriver.cd/v4/pipelines -v
